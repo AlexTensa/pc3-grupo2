390e7e2c69e2afc357002c116ddd293df91cbd62
AlexTensa <alextensa@hotmail.com>
2023-06-05 11:28:24 +0200

    codigo1

---
+++ pc3-grupo2/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+
+
+
+
+
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [AngularFireModule.initializeApp(environment.ts), AngularFireAuthModule ,BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ pc3-grupo2/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  apiKey: "AIzaSyC8EjztWYyOAVC3THoWnhaNBuqPgZglYuc",
+  authDomain: "pc3-grupo2.firebaseapp.com",
+  projectId: "pc3-grupo2",
+  storageBucket: "pc3-grupo2.appspot.com",
+  messagingSenderId: "683478891087",
+  appId: "1:683478891087:web:eeef571518e6aa8883083a",
+  measurementId: "G-M88VS1040Y"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
